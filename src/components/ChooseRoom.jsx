2c6319476473f86836ed69c323cc0194eb022b9a
akolatch <aaron.kolatch@gmail.com>
2021-03-26 13:09:39 -0400

    style clean up

--- src/components/ChooseRoom.jsx
+++ src/components/ChooseRoom.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ChooseRoom = ({ roomList, selectRoom }) => (
-  <div id='room'>
+  <div>
     <label htmlFor='select'>Rooms: </label>
     <select name='' id='select' onChange={selectRoom}>
       <option value=''>All</option>
